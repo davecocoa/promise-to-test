c9ff95397fbbfc0781af7aa0546870378a472712
David McMullin <david.mcmullin@gmail.com>
2015-01-24 14:06:33 +0000

    Cleanup broken ref to justWait

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 var q = require('q');
-module.exports = {
-  waitFor: function(condition, timeout, frequency){
-    timeout = timeout || 1000
-    frequency = frequency || 10
-    var now = new Date().getTime()
-    
-    var try = function(error){
-      if((new Date().getTime() - now) > timeout) { throw error }
-      return this.justWait(frequency)
-                .then(condition)
-                .then(null, try)
-    }.bind(this)
-    
-    return try()
-  },
-  justWait: function(timeout){
-    return new q.promise(function(resolve, reject){
-      setTimeout(resolve, timeout)
-    })
+
+function waitFor(condition, timeout, frequency){
+  timeout = timeout || 1000
+  frequency = frequency || 10
+  var now = new Date().getTime()
+
+  var check = function(error){
+    if((new Date().getTime() - now) > timeout) { throw error }
+    return justWait(frequency)
+             .then(condition)
+             .then(null, check)
   }
+
+  return check()
+}
+
+function justWait(timeout){
+  return new q.promise(function(resolve, reject){
+    setTimeout(resolve, timeout)
+  })
+}
+
+
+module.exports = {
+  waitFor: waitFor,
+  justWait: justWait
 }
